dd601644b2f1087c471c2095a82d0be2d85eb7b6
Fox-2019 <152862877+Fox-2019@users.noreply.github.com>
2024-03-11 23:02:03 +0000

    Update sketch.js

--- TESTING ENEMIES - UNFINISHED/sketch.js
+++ TESTING ENEMIES - UNFINISHED/sketch.js
@@ -4,88 +4,88 @@
 
 
 // Enemies
-let bowser;
-let goombas = [];
-let goombaSize = 80;
-let numGoombas = 5;
+//let bowser;
+//let goombas = [];
+//let goombaSize = 80;
+//let numGoombas = 5;
 
 // enemy speed variable 
-var speedX = 2.5;
-var speedY = 1.6;
-var x;
-var y;
+//var speedX = 2.5;
+//var speedY = 1.6;
+//var x;
+//var y;
 
 
 // sprites
-let bowserSprite;
-let goombaSprite;
+//let bowserSprite;
+//let goombaSprite;
 
- function preload(){
-  goombaSprite = loadImage("pinky.png");
-  bowserSprite = loadImage("dragon.png");
+// function preload(){
+//  goombaSprite = loadImage("pinky.png");
+//  bowserSprite = loadImage("dragon.png");
  }
 
 
-function setup() {
- createCanvas(500, 500);
- x= width/2;
- y = height/2;
+//function setup() {
+// createCanvas(500, 500);
+// x= width/2;
+// y = height/2;
 
 
- bowser = new Enemy(bowserSprite, 250, 250, 99);
+// bowser = new Enemy(bowserSprite, 250, 250, 99);
  
 
  // FOR LOOP TO CREATE GOOMBAS - store goombas in array
- for ( let i = 0; i < 100; i++){
-   goombas[i] =  new Enemy(goombaSprite,
-                          random(0, width),
-                          random(0, height),
-                          goombaSize)
+// for ( let i = 0; i < 100; i++){
+//   goombas[i] =  new Enemy(goombaSprite,
+                       //   random(0, width),
+                       //   random(0, height),
+                      //    goombaSize)
  }
 
  
    }
 
-function draw(){ 
-background(245);
+//function draw(){ 
+//background(245);
 
-x = x + random(-speedX, speedX);
-y = y + random(-speedY, speedY);
-(x, y, 20, 20);
+//x = x + random(-speedX, speedX);
+//y = y + random(-speedY, speedY);
+//(x, y, 20, 20);
 
-bowser.display();
+//bowser.display();
 
 
 
 // FOR LOOP TO DISPLAY GOOMBAS - Access instances from array created in setup()
 
-for ( let i = 0; i< numGoombas; i++){
-  goombas[i].display();
+//for ( let i = 0; i< numGoombas; i++){
+//  goombas[i].display();
   
 
 }
 }
 
 
-class Enemy{// capitalise class names
-  constructor(sprite, x, y, size, points){
-    this.sprite=sprite;
-    this.x=x;
-    this.y=y;
-    this.size = size;
-    this.speed = 5;
+//class Enemy{// capitalise class names
+ // constructor(sprite, x, y, size, points){
+//    this.sprite=sprite;
+//    this.x=x;
+//    this.y=y;
+ //   this.size = size;
+  //  this.speed = 5;
 
     
   
   }
-  display(){
-    image(this.sprite, this.x, this.y, this.size);
+//  display(){
+ //   image(this.sprite, this.x, this.y, this.size);
   }
 }
 
 
 // class move funtion to get the enemies to move
-class move {
+//class move {
 
 
 }
