1fa5518f3a779785e34995142fe9bfc3a2dca6cd
Ishmaam Imtiaz <ishmaam2005@gmail.com>
2024-03-18 11:38:01 +0000

    Player spawns but not moving - wip

--- player.js
+++ player.js
@@ -1,12 +1,11 @@
 class Player {
   constructor(sprites, startAcross, startDown, size, speed, tileRules) {
     this.sprites = sprites;
-    // this.currentSprite = this.sprites.right;
     this.across = startAcross;
     this.down = startDown;
     this.x = this.across * size;
     this.y = this.down * size;
-    this.tileSize = size;
+    this.size = size; // Added size parameter
     this.speed = speed;
     this.tileRules = tileRules;
     this.dirX = 0;
@@ -16,76 +15,61 @@ class Player {
     this.tY = this.y;
   }
 
-    update() {
-      this.x = mouseX;
-      this.y = mouseY;
-    }  
-
-    
-    //Player Interactipn with collectible.
-    intersects(collectible) {
-        let d = dist(this.x, this.y, collectible.x, collectible.y);
-        return d < this.size / 2 + collectible.size / 2;
-}
-  
+  update() {
+    // Update player position
+    this.move();
+    this.display();
   }
 
-  setDirection(); {
+  intersects(collectible) {
+    // Check intersection with collectible
+    let d = dist(this.x, this.y, collectible.x, collectible.y);
+    return d < this.size / 2 + collectible.size / 2;
+  }
+
+  setDirection() {
+    // Set direction based on key input
     if (!this.isMoving) {
       if (key === "w") {
         this.dirX = 0;
         this.dirY = -1;
-        // this.currentSprite = this.sprites.up;
-      }
-
-      if (key === "s") {
+      } else if (key === "s") {
         this.dirX = 0;
         this.dirY = 1;
-        // this.currentSprite = this.sprites.down;
-      }
-
-      if (key === "a") {
+      } else if (key === "a") {
         this.dirX = -1;
         this.dirY = 0;
-        // this.currentSprite = this.sprites.left;
-      }
-
-      if (key === "d") {
+      } else if (key === "d") {
         this.dirX = 1;
         this.dirY = 0;
-        // this.currentSprite = this.sprites.right;
       }
-
       this.checkTargetTile();
     }
   }
 
-  checkTargetTile(); {
-    this.across = floor(this.x / this.tileSize);
-    this.down = floor(this.y / this.tileSize);
-
+  checkTargetTile() {
+    // Check if target tile is walkable
+    this.across = floor(this.x / this.size);
+    this.down = floor(this.y / this.size);
     let nextTileHorizontal = this.across + this.dirX;
     let nextTileVertical = this.down + this.dirY;
 
-    //check if tile within map
     if (
       nextTileHorizontal >= 0 &&
       nextTileHorizontal < mapSize &&
       nextTileVertical >= 0 &&
       nextTileVertical < mapSize
     ) {
-      //if it is, check if targetTile walkable
       if (this.tileRules[nextTileVertical][nextTileHorizontal] != 1) {
-        this.tX = nextTileHorizontal * this.tileSize;
-        this.tY = nextTileVertical * this.tileSize;
-
-        camera.moveIfOffscreen();
+        this.tX = nextTileHorizontal * this.size;
+        this.tY = nextTileVertical * this.size;
         this.isMoving = true;
       }
     }
   }
 
-  move(); {
+  move() {
+    // Move player
     if (this.isMoving) {
       this.x += this.speed * this.dirX;
       this.y += this.speed * this.dirY;
@@ -98,9 +82,11 @@ class Player {
     }
   }
 
-  display(); {
-    image(this.sprites, this.x, this.y, this.tileSize, this.tileSize);
+  display() {
+    // Display player sprite
+    image(this.sprites, this.x, this.y, this.size, this.size);
   }
+}
 
   debug(isON); {
     if (isON) {
