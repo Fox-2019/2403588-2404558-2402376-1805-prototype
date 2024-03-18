72228659d7495462d4648ca3fe4da46dcdc4c9ac
Jasveenanand5 <152862757+Jasveenanand5@users.noreply.github.com>
2024-03-18 15:35:16 +0000

    dragon chaser

--- sketch.js
+++ sketch.js
@@ -1,17 +1,62 @@
-// Load dragon image
-let dragonImage;
+//EMERALD CLASS
+class Emerald {
+  constructor(image, x, y, size) {
+    this.image = image;
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.isCollected = false;
+  }
 
-// Declare Dragon object
+  display() {
+    if (!this.isCollected) {
+      image(this.image, this.x * tileSize, this.y * tileSize, this.size, this.size);
+    }
+  }
+
+  checkCollision(player) {
+    if (!this.isCollected && dist(player.x, player.y, this.x * tileSize, this.y * tileSize) < tileSize / 2) {
+      this.isCollected = true;
+      increasePoints(2);
+      respawnEmeralds();
+    }
+  }
+}
+
+// Declare Emerald objects array and load its image
+let emeralds = [];
+let emeraldImage;
+
+//TILEMAPS
+let tileSize = 50; //pixel size of tiles
+let mapSize = 30; // n x n size of the tilemap
+let tilemap = []; // contains each tile Object: outer array is Y inner arrays are Xs; tilemap[y][x]
+let textures = [];
+let textureMap = []; // same as tilemap but this determines which tile graphic is shown
+
+//PLAYER
+let player;
+let playerSprite;
+let playerSpeed = 5;
+let playerSize = tileSize;
+
+//DRAGON
 let dragon;
+let dragonImage;
+let dragonSpeed = 2; // Initial speed of the dragon
 
-// Initialize dragon speed
-let dragonSpeed = 2;
+let camera;
+
+let debugFLIP = false; //true turns on all debug functions
+
+//Point system variables
+let points = 0;
 
 function preload() {
   textures[0] = loadImage("leafy.png");
   textures[1] = loadImage("crystal.png");
   playerSprite = loadImage("fairy.png");
-  emeraldImage = loadImage("emerald.png");
+  emeraldImage = loadImage("emerald.png"); // Load emerald image
   dragonImage = loadImage("dragon.png"); // Load dragon image
 }
 
@@ -22,13 +67,13 @@ function setup() {
   camera = new Camera();
   player = new Player(playerSprite, floor(random(0, 10)), floor(random(0, 10)), playerSize, playerSpeed, textureMap);
 
-  // Initialize Dragon object
-  dragon = new Dragon(dragonImage, floor(random(0, 10)), floor(random(0, 10)), playerSize, dragonSpeed, textureMap);
-
-  // Initialize emeralds
-  for (let i = 0; i < 2; i++) {
+  // Initialize Emerald objects array with random positions
+  for (let i = 0; i < 4; i++) { // Initially spawn 4 emeralds
     spawnEmerald();
   }
+
+  // Initialize the dragon
+  dragon = new Dragon(dragonImage, floor(random(0, 10)), floor(random(0, 10)), playerSize, dragonSpeed, textureMap);
 }
 
 function draw() {
@@ -51,6 +96,66 @@ function draw() {
   // Check collision between player and dragon
   if (dist(player.x, player.y, dragon.x, dragon.y) < tileSize / 2) {
     gameOver();
+  }
+}
+
+function DisplayGraphics() {
+  translate(camera.Xtranslate + camera.Xoffset, camera.Ytranslate + camera.Yoffset);
+  for (let across = 0; across < mapSize; across++) {
+    for (let down = 0; down < mapSize; down++) {
+      tilemap[across][down].display();
+      tilemap[across][down].debug(debugFLIP);
+    }
+  }
+  player.display();
+  player.debug(debugFLIP);
+}
+
+function GenerateTextureMap() {
+  for (let y = 0; y < mapSize; y++) {
+    textureMap[y] = [];
+    for (let x = 0; x < mapSize; x++) {
+      let tileGen = floor(random(1, 11));
+      if (tileGen > 1) tileGen = 0;
+      textureMap[y][x] = tileGen;
+    }
+  }
+}
+
+function GenerateTileMap() {
+  let id = 0;
+  for (let across = 0; across < mapSize; across++) {
+    tilemap[across] = [];
+    for (let down = 0; down < mapSize; down++) {
+      tilemap[across][down] = new Tile(textureMap[down][across], across, down, tileSize, id);
+      id++;
+    }
+  }
+}
+
+function increasePoints(amount) {
+  points += amount;
+}
+
+function keyPressed() {
+  camera.SetCamDir();
+  player.setDirection();
+}
+
+function spawnEmerald() {
+  let x, y;
+  do {
+    x = floor(random(0, mapSize));
+    y = floor(random(0, mapSize));
+  } while (textureMap[y][x] === 1); // Check if the position overlaps with a crystal tile
+
+  emeralds.push(new Emerald(emeraldImage, x, y, tileSize));
+}
+
+function respawnEmeralds() {
+  emeralds = []; // Clear existing emeralds
+  for (let i = 0; i < 4; i++) { // Respawn 4 emeralds
+    spawnEmerald();
   }
 }
 
@@ -80,14 +185,6 @@ class Dragon {
   }
 }
 
-function increasePoints(amount) {
-  points += amount;
-  // Increase dragon speed every 10 points
-  if (points % 10 === 0) {
-    dragon.speed += 0.5;
-  }
-}
-
 function gameOver() {
   textSize(32);
   textAlign(CENTER);
@@ -95,7 +192,3 @@ function gameOver() {
   text("Game Over", width / 2, height / 2);
   noLoop(); // Stop the game loop
 }
-
-
-
-
