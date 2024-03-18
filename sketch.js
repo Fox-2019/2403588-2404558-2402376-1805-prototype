496ef7974f3768081f8b42e8fb4be832b5ab2cfd
Jasveenanand5 <152862757+Jasveenanand5@users.noreply.github.com>
2024-03-18 14:18:04 +0000

    might work pick

--- sketch.js
+++ sketch.js
@@ -1,3 +1,31 @@
+//EMERALD CLASS
+class Emerald {
+  constructor(image, x, y, size) {
+    this.image = image;
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.isCollected = false;
+  }
+
+  display() {
+    if (!this.isCollected) {
+      image(this.image, this.x * tileSize, this.y * tileSize, this.size, this.size);
+    }
+  }
+
+  checkCollision(player) {
+    if (!this.isCollected && dist(player.x, player.y, this.x, this.y) < tileSize / 2) {
+      this.isCollected = true;
+      increasePoints(2);
+    }
+  }
+}
+
+// Declare Emerald object and load its image
+let emerald;
+let emeraldImage;
+
 //TILEMAPS
 let tileSize = 50; //pixel size of tiles
 let mapSize = 30; // n x n size of the tilemap
@@ -7,7 +35,6 @@ let textureMap = []; // same as tilemap but this determines which tile graphic i
 
 //PLAYER
 let player;
-// let playerSprite = {};
 let playerSprite;
 let playerSpeed = 5;
 let playerSize = tileSize;
@@ -17,75 +44,45 @@ let camera;
 let debugFLIP = false; //true turns on all debug functions
 
 //Point system variables
-
-let points = 0
+let points = 0;
 
 function preload() {
   textures[0] = loadImage("leafy.png");
   textures[1] = loadImage("crystal.png");
-
   playerSprite = loadImage("fairy.png");
-  //for when we have original textures for the character
-  // playerSprite = {
-  //   up: loadImage("imgs/librarian-u.png"),
-  //   down: loadImage("imgs/librarian-d.png"),
-  //   left: loadImage("imgs/librarian-l.png"),
-  //   right: loadImage("imgs/librarian-r.png"),
-  // };
+  emeraldImage = loadImage("emerald.png"); // Load emerald image
 }
 
 function setup() {
   createCanvas(550, 550);
-  // imageMode(CENTER);
-
-  //generates the n x n map to determine where obstacle wall tiles are located
   GenerateTextureMap();
-
-  //uses the generated texture map to create a 'tile map' and create the objects for each tile
   GenerateTileMap();
-  // console.log(tilemap);
-
   camera = new Camera();
-  player = new Player(
-    playerSprite,
-    floor(random(0, 10)),
-    floor(random(0, 10)),
-    playerSize,
-    playerSpeed,
-    textureMap
-  );
-} // END OF SETUP
+  player = new Player(playerSprite, floor(random(0, 10)), floor(random(0, 10)), playerSize, playerSpeed, textureMap);
+  emerald = new Emerald(emeraldImage, floor(random(0, mapSize)), floor(random(0, mapSize)), tileSize); // Initialize Emerald object
+}
 
 function draw() {
   background(50);
-
   DisplayGraphics();
   player.move();
   camera.move();
-
-textAlign(RIGHT);
-textSize(20);
-fill(255);
-text("points:"+points,width-50,30);
-
-
-} //END OF DRAW
-
-
+  textAlign(RIGHT);
+  textSize(20);
+  fill(255);
+  text("points:" + points, width - 50, 30);
+  emerald.display();
+  emerald.checkCollision(player);
+}
 
 function DisplayGraphics() {
-  translate(
-    camera.Xtranslate + camera.Xoffset,
-    camera.Ytranslate + camera.Yoffset
-  );
-
+  translate(camera.Xtranslate + camera.Xoffset, camera.Ytranslate + camera.Yoffset);
   for (let across = 0; across < mapSize; across++) {
     for (let down = 0; down < mapSize; down++) {
       tilemap[across][down].display();
       tilemap[across][down].debug(debugFLIP);
     }
   }
-
   player.display();
   player.debug(debugFLIP);
 }
@@ -93,12 +90,9 @@ function DisplayGraphics() {
 function GenerateTextureMap() {
   for (let y = 0; y < mapSize; y++) {
     textureMap[y] = [];
-
     for (let x = 0; x < mapSize; x++) {
-      //whether tile is walkable or not is choosen randomly, 1 in 10 chance it'll be a stone
       let tileGen = floor(random(1, 11));
       if (tileGen > 1) tileGen = 0;
-
       textureMap[y][x] = tileGen;
     }
   }
@@ -106,31 +100,21 @@ function GenerateTextureMap() {
 
 function GenerateTileMap() {
   let id = 0;
-  // X loop
   for (let across = 0; across < mapSize; across++) {
     tilemap[across] = [];
-    // Y loop
     for (let down = 0; down < mapSize; down++) {
-      tilemap[across][down] = new Tile(
-        textureMap[down][across],
-        across,
-        down,
-        tileSize,
-        id
-      );
-
+      tilemap[across][down] = new Tile(textureMap[down][across], across, down, tileSize, id);
       id++;
     }
   }
 }
-//Not complete version need to link enemy code to the point system
-//Function to increase points
 
-function increasePoints(amount){
-  points+=amount;
+function increasePoints(amount) {
+  points += amount;
 }
 
 function keyPressed() {
   camera.SetCamDir();
   player.setDirection();
 }
+
